87ad2663f4e40863f4dbaff9b59c7d9b179b777b
Nikesuiden <taiki.nikewp@gmail.com>
2024-06-15 02:57:29 +0900

    最低限のUIを整備

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,57 @@
-import { useState } from 'react';
+import { use, useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
-  const[sampleVal, setSampleVal] = useState(0);
+  const [sampleVal, setSampleVal] = useState(0);
+  const [faceBotton, setFaceBotton] = useState(0);
+  const [bombMap, setBombMap] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
   console.log(sampleVal);
 
   return (
     <div className={styles.container}>
-      <div className={styles.sampleStyle}
-      style={{backgroundPosition: `${(sampleVal * -30)}px 0`}}
-      />
-      <button onClick={() => setSampleVal(Val => (Val+1)%14)}>Sample</button>
+      <div className={styles.selectMode} />
+      <div className={styles.topContainer}>
+        <div className={styles.flagCounter} />
+        <div className={styles.resetBotton} style={{ backgroundPosition: `-330px 0` }} />
+        <div className={styles.timeCounter} />
+      </div>
+      <div className={styles.boardstyle}>
+        {bombMap.map(
+          (
+            row,
+            y, //map = for
+          ) =>
+            row.map(
+              (
+                color,
+                x, //row
+              ) => (
+                <div
+                  className={styles.cellstyle}
+                  key={`${x}-${y}`}
+                  onClick={() => clickHandler(x, y)}
+                >
+                  {color !== 0 && (
+                    <div
+                      className={styles.stoneStyle}
+                      style={{ background: color === 1 ? '#000' : '#fff' }}
+                    />
+                  )}
+                </div>
+              ),
+            ),
+        )}
+      </div>
     </div>
   );
 };
