6fe0e3344d211eb840035908995f573834038c11
Nikesuiden <160406814+Nikesuiden@users.noreply.github.com>
2024-06-24 03:12:14 +0900

    タイム機能実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect ,useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
@@ -15,21 +15,8 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
 
-  // 数字やボムを隠す岩カバー
-  const [coverState, setCoverState] = useState([
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  ])
-
   const [bombMap, setBombMap] = useState([
-    // 0がボムなし、1がボムあり、以降周辺のボム数2~9:1~8
+    // 0がボムなし、1がボムあり、以降周辺のボム数2~10:0~8
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -52,16 +39,55 @@ const Home = () => {
     [-1, 1],
   ];
 
-  // ここで爆弾を配置する関数を定義する必要はなくなるので削除
+  const [time, setTime] = useState(0); // タイムの状態を追加
+  const [isStarted, setIsStarted] = useState(false); // タイマー開始判定
 
-  // 列数の定義
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+
+    if (isStarted && time < 999) {
+      timer = setInterval(() => {
+        setTime((prevTime) => {
+          if (prevTime < 999) {
+            return prevTime + 1;
+          } else {
+            clearInterval(timer);
+            return prevTime;
+          }
+        });
+      }, 1000); // 1秒ごとにカウントアップ
+    }
+
+    // コンポーネントがアンマウントされたときにタイマーをクリア
+    return () => clearInterval(timer);
+  }, [isStarted, time]);
+
   const rowLength = bombMap[0];
+  const newBompMap = structuredClone(bombMap);
+  const newClickMap = structuredClone(clickState);
+
+  let bombCounter = 2; // デフォルト１だとボムに変化する可能性があるため
+
+  // 呼び出し関数として blank を定義
+  const blank = (x: number, y: number) => {
+    // クリックしたセルのボムカウントが0だった場合、周辺8方向のセルもクリック済みにする
+    console.log(`クリックセルの周辺ボム数${newBompMap[x][y]}`);
+    if (newBompMap[x][y] === 0) {
+      for (const dir of directions) {
+        // もし周辺セルが範囲外ではなかった場合
+        if (
+          newBompMap[x + dir[0]] !== undefined &&
+          newBompMap[x + dir[0]][y + dir[1]] !== undefined
+        ) {
+          // クリック済みにする
+          newClickMap[x + dir[0]][y + dir[1]] = 1;
+        }
+      }
+    }
+  };
 
   const clickHandler = (x: number, y: number) => {
     console.log(`クリックした座標 [x, y] => [${x}, ${y}]`);
-    const newBompMap = structuredClone(bombMap);
-    const newClickMap = structuredClone(clickState);
-    let bombCounter = 2; // デフォルト１だとボムに変化する可能性があるため
 
     // 二次元配列を一次元化する配列
     const oneDimArray: number[] = newClickMap.flat(1);
@@ -70,6 +96,9 @@ const Home = () => {
 
     // もしファーストクリックだったら
     if (countOfValue === 0) {
+      // ファーストクリックでタイマー開始
+      setIsStarted(true);
+
       newBompMap[x][y] = 0;
 
       // マップ上全展開、残りのセルに爆弾を設置
@@ -90,39 +119,35 @@ const Home = () => {
         }
       }
 
-
       // マップ内の１の数が totalOnes と一致するまで繰り返す
       while (true) {
+        // フラットなリストをシャッフルする
+        for (let i = flatMap.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [flatMap[i], flatMap[j]] = [flatMap[j], flatMap[i]];
+        }
 
-      // フラットなリストをシャッフルする
-      for (let i = flatMap.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [flatMap[i], flatMap[j]] = [flatMap[j], flatMap[i]];
-      }
+        // シャッフルされたリストを新しい bombMap に適用
+        let index = 0;
+        for (let n = 0; n < bombMap.length; n++) {
+          for (let m = 0; m < rowLength.length; m++) {
+            newBompMap[n][m] = flatMap[index++];
+          }
+        }
 
-      // シャッフルされたリストを新しい bombMap に適用
-      let index = 0;
-      for (let n = 0; n < bombMap.length; n++) {
-        for (let m = 0; m < rowLength.length; m++) {
-          newBompMap[n][m] = flatMap[index++];
+        // クリックした場所はボムなしにする。
+        if (newBompMap[x][y] === 1) {
+          newBompMap[x][y] = 0;
+        }
+
+        const newBombArray: number[] = newBompMap.flat(1);
+        const countNewOne = newBombArray.filter((item) => item === 1).length;
+
+        if (countNewOne === totalOnes) {
+          console.log(`ボムの数${countNewOne}`);
+          break;
         }
       }
-
-      // クリックした場所はボムなしにする。
-      if (newBompMap[x][y] === 1) {
-        newBompMap[x][y] = 0;
-      }
-
-      const newBombArray: number[] = newBompMap.flat(1);
-      const countNewOne = newBombArray.filter((item) => item === 1).length;
-
-      if (countNewOne === totalOnes) {
-        console.log(`ボムの数${countNewOne}`)
-        break;
-      }
-
-    }
-
 
       // セルが空白の場合、周辺のボム数を計測
       for (let n = 0; n < bombMap.length; n++) {
@@ -135,7 +160,6 @@ const Home = () => {
                 newBompMap[n + dir[0]][m + dir[1]] === 1
               ) {
                 bombCounter += 1;
-                console.log(bombCounter - 2);
               }
             }
             newBompMap[n][m] = bombCounter;
@@ -148,10 +172,29 @@ const Home = () => {
     // クリックしたところはクリック済みの "1" 印を設置
     newClickMap[x][y] = 1;
 
+    // 空白セルの周囲を探索
+
+
     // 二次元配列を一次元化する配列
     const oneDimArray2: number[] = newClickMap.flat(1);
     // フラット化された関数から "1" の値をカウントする
     const countOfValue2 = oneDimArray2.filter((item) => item === 1).length;
+
+    const seeNewBompMap = [];
+
+    for (let n = 0; n < bombMap.length; n++) {
+      for (let m = 0; m < rowLength.length; m++) {
+        if (newBompMap[n][m] >= 2) {
+          seeNewBompMap.push(newBompMap[n][m] - 2);
+        } else if (newBompMap[n][m] === 1) {
+          seeNewBompMap.push("b");
+        }
+      }
+    }
+
+    blank(x, y);
+
+    console.log(`アメリカの${seeNewBompMap}, 計${seeNewBompMap.length}`);
 
     console.log(oneDimArray2);
     console.log(countOfValue2);
@@ -161,38 +204,54 @@ const Home = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.selectMode}>
-        初級 中級 上級 カスタム
-        <div className={styles.gameContainer}>
-          <div className={styles.topContainer}>
-            <div className={styles.flagCounter} />
-            <div className={styles.resetBotton} style={{ backgroundPosition: `-330px 0` }} />
-            <div className={styles.timeCounter} />
-          </div>
-          <div className={styles.boardstyle}>
-            {bombMap.map((row, y) =>
-              row.map((bomb, x) => (
+      <div className={styles.selectMode}>初級 中級 上級 カスタム</div>
+      <div className={styles.gameContainer}>
+        <div className={styles.topContainer}>
+          <div className={styles.flagCounter} />
+          <div className={styles.resetBotton} style={{ backgroundPosition: `-330px 0` }} />
+          <div className={styles.timeCounter}>{time.toString().padStart(3, '0')}</div>
+        </div>
+        <div className={styles.boardstyle}>
+          {bombMap.map((row, y) =>
+            row.map((bomb, x) => {
+              const clickValue = clickState[x][y]; // clickState から現在のセルの状態を取得
+              return (
                 <div
                   className={styles.cellstyle}
                   key={`${x}-${y}`}
                   onClick={() => clickHandler(x, y)}
                 >
-                  {bomb === 1 && (
-                    <div
-                      className={styles.bombStyle}
-                      style={{ backgroundPosition: bomb === 1 ? `-300px 0` : `100px 0` }}
-                    />
+                  {/* クリックされていない場合 */}
+                  {clickValue === 0 && <div className={styles.coverstyle} />}
+
+                  {/* クリックされている場合 */}
+                  {clickValue === 1 && (
+                    <>
+                      {bomb === 1 && (
+                        <div
+                          className={styles.bombStyle}
+                          style={{ backgroundPosition: `-300px 0` }}
+                        />
+                      )}
+                      {bomb >= 3 && (
+                        <div
+                          className={styles.countStyle}
+                          style={{ backgroundPosition: `${-30 * (bomb - 3)}px 0` }}
+                        />
+                      )}
+                      {/* ボムがなく、周りにボムもない場合 */}
+                      {bomb === 0 && <div className={styles.emptyCell} />}
+                    </>
                   )}
-                  {bomb >= 3 && (
-                    <div
-                      className={styles.countStyle}
-                      style={{ backgroundPosition: `${-30 * (bomb - 3)}px 0` }}
-                    />
+
+                  {/* フラグが立っている場合 */}
+                  {clickValue === 2 && (
+                    <div className={styles.countStyle} style={{ backgroundPosition: `-270px 0` }} />
                   )}
                 </div>
-              ))
-            )}
-          </div>
+              );
+            }),
+          )}
         </div>
       </div>
     </div>
